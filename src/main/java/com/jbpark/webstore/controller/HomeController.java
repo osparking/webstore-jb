895cb159f19a0ee01decf97886ba9d2ce4a38c01
Park, JongBum <jbpark03@gmail.com>
2021-04-08 15:10:36 +0900

    root request forwarded to greeting()

--- src/main/java/com/jbpark/webstore/controller/HomeController.java
+++ src/main/java/com/jbpark/webstore/controller/HomeController.java
@@ -9,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping
 	public String welcome(Model model) {
-		model.addAttribute("greeting", "안녕");
-		model.addAttribute("tagline", "웹스트어");
+		model.addAttribute("greeting", "안녕!");
+		model.addAttribute("tagline", "웹스토어");
+		return "forward:/welcome/greeting";
+//		return "redirect:/welcome/greeting";
+	}
+
+	@RequestMapping("/welcome/greeting")
+	public String greeting(Model model) {
+		model.addAttribute("tagline", "더조은학원");
 		return "welcome";
 	}
 }
